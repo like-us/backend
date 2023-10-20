8d9e0b29fe0b5a3208a57a7ae8787060d2846ad4
mugishayves <mugishayves189000@gmail.com>
2023-10-20 12:06:02 +0200

    removed the unnecessary uploaders

--- src/api/validations/new.validations.ts
+++ src/api/validations/new.validations.ts
@@ -2,10 +2,12 @@ import Joi from 'joi'
 export default {
 	newNews: Joi.object({
 		title: Joi.string().required(),
+		shortDescription: Joi.string().required(),
 		body: Joi.string().required(),
 	}),
 	updateNews: Joi.object({
 		title: Joi.string().optional(),
+		shortDescription: Joi.string().optional(),
 		body: Joi.string().optional(),
 	}),
 }
