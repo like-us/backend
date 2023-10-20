83f1428a0b941e4181987ffb1dc0d82b085f23e7
mugishayves <mugishayves189000@gmail.com>
2023-10-20 11:04:54 +0200

    made the contact validator

--- src/api/validations/contact.validations.ts
+++ src/api/validations/contact.validations.ts
@@ -1,21 +1,10 @@
 import Joi from 'joi'
 export default {
-	newUser: Joi.object({
-		name: Joi.string().required(),
-		username: Joi.string().required(),
-		email: Joi.string().email().required(),
-		password: Joi.string().required(),
-	}),
-	loginUser: Joi.object({
-		email: Joi.string().email().required(),
-		password: Joi.string().required(),
-	}),
-	updateUser: Joi.object({
-		name: Joi.string().optional(),
-		username: Joi.string().optional(),
-		email: Joi.string().email().optional(),
-		password: Joi.string().optional(),
-		bio: Joi.string().optional(),
-		phone: Joi.string().optional(),
-	}),
+    createContact: Joi.object({
+        name: Joi.string().required(),
+        phone: Joi.string().required(),
+        email: Joi.string().email().required(),
+        address: Joi.string().email().required(),
+        message: Joi.string().required(),
+    }),
 }
