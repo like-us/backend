f996f932c7c479a0abe1f01fed72b106b52c2478
mugishayves <mugishayves189000@gmail.com>
2023-10-20 08:51:53 +0200

    solved the createNew error

--- src/api/services/news.service.ts
+++ src/api/services/news.service.ts
@@ -9,7 +9,7 @@ const getAllNew = async () => {
     return ANew.find({})
 }
 const createNew = async (body: any) => {
-    const newUser = new ANew({link:body?.link,category:"",title:body?.title || '' ,image:'',shortDescription:body?.shortDescription || '',body:body?.body || body?.content || ''});
+    const newUser = new ANew({link:body?.link,category:"string",title:body?.title || '' ,image:'string',shortDescription:body?.shortDescription || '',body:body?.body || body?.content || ''});
     return newUser.save()
 }
 const updateNew = (id: string, body: any) => {
