22ccc08b423839b48e5ef834b5b8c00ec12cd0a8
mugishayves <mugishayves189000@gmail.com>
2023-10-20 11:21:39 +0200

    removed unnecessary imports from services exporter

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,4 +1,4 @@
-export { default as userService } from './testimonial.service'
 export { default as authService } from './auth.service'
+export { default as contactService } from './contact.service'
 export { default as newService } from './news.service'
 export { default as testimonialService } from './testimonial.service'
