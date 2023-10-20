d75e42e0a669d437ec1f48c613a71bd3f10e16cd
mugishayves <mugishayves189000@gmail.com>
2023-10-20 12:05:26 +0200

    removed the unnecessary uploaders

--- src/api/validations/testimonial.validations.ts
+++ src/api/validations/testimonial.validations.ts
@@ -2,10 +2,18 @@ import Joi from 'joi'
 export default {
 	newTestimonial: Joi.object({
 		title: Joi.string().required(),
+		name: Joi.string().required(),
+		email: Joi.string().required(),
+		phone: Joi.string().required(),
+		address: Joi.string().required(),
 		body: Joi.string().required(),
 	}),
 	updateTestimonial: Joi.object({
 		title: Joi.string().optional(),
+		name: Joi.string().optional(),
+		email: Joi.string().optional(),
+		phone: Joi.string().optional(),
+		address: Joi.string().optional(),
 		body: Joi.string().optional(),
 	}),
 }
